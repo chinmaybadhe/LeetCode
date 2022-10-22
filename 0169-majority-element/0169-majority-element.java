3911fe87aa5dbef27f336e2f54e6792f6847eae1
Chinmay Badhe <98736290+chinmaybadhe@users.noreply.github.com>
2022-10-21 17:40:40 -0700

    Time: 2 ms (92.90%), Space: 56.3 MB (37.95%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        
+        int current=nums[0];
+        int count=1;
+        
+        for(int i=1;i<nums.length;i++){
+            
+            if(nums[i]==current){
+                count++;
+                
+            }
+            else{
+                count--;
+                
+            }
+            if(count==0){
+                current=nums[i];
+                count=1;
+                
+            }
+            
+        }
+            
+        return current;
+    }
+}
